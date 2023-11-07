970ae51193a5658bd41d67a5b8851d27eee4af7a
David <dpthibodeau@umass.edu>
2023-11-06 22:13:19 -0500

    data augmentation

--- load_data.py
+++ load_data.py
@@ -4,6 +4,8 @@ from torch.utils.data import TensorDataset, DataLoader, Dataset
 import torchvision.transforms as T
 import torch
 from skimage.transform import resize
+from PIL import Image
+from scipy import ndimage
 
 transform = T.Compose([
                 T.ToTensor(),
@@ -16,7 +18,21 @@ class LFWDataLoader:
         lfw_people = fetch_lfw_people(data_home='datasets', color=True, resize=1, download_if_missing=True)
         data = lfw_people.images
         data = np.moveaxis(data, 3, 1)
-        train, val, test = np.split(data,[int(train_size*len(data)), int((train_size+val_size)*(len(data)))])
+        train, val, test = np.split(data, [int(train_size*len(data)), int((train_size+val_size)*(len(data)))])
+        train_copy = train.copy()
+        train1, train2, train3, train4, train5 = np.split(train_copy, [int(.2*len(train)), int(.4*(len(train))), int(.6*len(train)), int(.8*len(train))])
+
+        for i in range(len(train1)):
+            for j in range(3):
+                train1[i][j] = ndimage.rotate(train1[i][j], 45, reshape=False, cval=1)
+                train2[i][j] = ndimage.rotate(train2[i][j], 90, reshape=False, cval=1)
+                train3[i][j] = ndimage.shift(train4[i][j], -30, order=0, cval=1)
+                train4[i][j] = ndimage.shift(train4[i][j], 30, order=0, cval=1)
+            train5[i] = ndimage.median_filter(train5[i], 3)
+        print(train.shape)
+        train = np.concatenate((train, train1, train2, train3, train4, train5), axis=0)
+        np.random.shuffle(train)
+
         print(train.shape)
         print(val.shape)
         print(test.shape)
@@ -30,4 +46,3 @@ class LFWDataLoader:
         test = torch.Tensor(test)
         self.test_dataset = TensorDataset(test)
         self.test_dataloader = DataLoader(self.test_dataset, batch_size=batch_size)
-        
